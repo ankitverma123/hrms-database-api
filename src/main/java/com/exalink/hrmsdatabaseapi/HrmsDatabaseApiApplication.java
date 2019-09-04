5e79f888825ba601797caa52da43ef3428754fd5
ankitkverma <ankitkverma@deloitte.com>
2019-09-04 09:58:04 +0530

    Initial Commit

---
+++ src/main/java/com/exalink/hrmsdatabaseapi/HrmsDatabaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.exalink.hrmsdatabaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsDatabaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsDatabaseApiApplication.class, args);
+	}
+
+}
